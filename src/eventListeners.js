044e46022c33004b95357a695983fd7f5dad3dc4
Christos Agoras <christos.agoras@camelotls.com>
2021-01-17 16:24:36 +0200

    step-4: capture event target for people and planet button clicks

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,7 +1,13 @@
 
 export const appEventListener = (event) => {
-  console.log('clicked')
   //🔨 4. verify that tabs are working 🔨
+  if (event.target.id === 'people') {
+    console.log('people clicked')
+  }
+
+  if (event.target.id === 'planets') {
+    console.log('planets clicked')
+  }
   //🔨 8. render people elements by clicking on people's tab 🔨
   //🔨 8. render planets elements by clicking on planets' tab🔨
   //🔨 11. render planet info element by clicking on show planet info button🔨
